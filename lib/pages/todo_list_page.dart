8ed0ed5b3aa13776d1484bde4360e3e469a082ae
Sl6yer <eduardoback2018@gmail.com>
2025-01-14 14:00:29 -0500

    first commit

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  TodoListPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(onPressed: login, child: Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login(){
+    String text = emailController.text;
+    print(text);
+    emailController.clear();
+  }
+
+}
